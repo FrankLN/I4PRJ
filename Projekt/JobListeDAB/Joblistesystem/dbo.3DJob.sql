42395b6fc6d3f003f807217a45d454de970a3e05
Magnus Schou Abildgren <magnusabildgren@ofir.dk>
2014-03-04 11:31:36 +0100

    Update a la Magnus

---
+++ Projekt/JobListeDAB/Joblistesystem/dbo.3DJob.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[3DJob]
+(
+	[OrdreId] BIGINT NOT NULL PRIMARY KEY, 
+    [Materiale] NVARCHAR(50) NOT NULL, 
+    [Ejer] NVARCHAR(50) NOT NULL, 
+    [Deadline] NVARCHAR(50) NULL DEFAULT NULL, 
+    [Fil] NVARCHAR(MAX) NOT NULL, 
+    [OprettelsesTidspunkt] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_3DJob_Materiale] FOREIGN KEY ([Materiale]) REFERENCES [Materiale]([MaterialeType]), 
+    CONSTRAINT [FK_3DJob_Bruger] FOREIGN KEY ([Ejer]) REFERENCES [Bruger]([Email])
+)
