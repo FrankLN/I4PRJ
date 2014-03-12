49255c17df69cc3cb376cd83d425438499204c56
UlverHvidt <ulver.hvidt@gmail.com>
2014-03-12 10:04:25 +0100

    Lad os se om dette virker

--- Projekt/JobListeDAB/DatabaseInterface/Database.cs
+++ Projekt/JobListeDAB/DatabaseInterface/Database.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Linq;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
@@ -10,6 +12,12 @@ namespace DatabaseInterface
 {
     public class Database
     {
+        //private SqlConnection conn;
+        Database()
+        {
+            //conn = new SqlConnection(@"Data Source=(localdb)\Projects;Initial Catalog=Joblistesystem;Integrated Security=True;");
+        }
+
        /* void AddUser(User user)
         {
             DataContext db = new DataContext(@"C:\Users\Søren Emil\AppData\Local\Microsoft\VisualStudio\SSDT\JobListeDAB\Joblistesystem.mdf");
@@ -19,8 +27,8 @@ namespace DatabaseInterface
 
         int ValidateLogInIndo(string email, string password)
         {
-            DataContext db = new DataContext(@"C:/Users/Søren Emil/AppData/Local/Microsoft/VisualStudio/SSDT/JobListeDAB/Joblistesystem.mdf");
-
+            DataContext db = new DataContext(@"C:\Users\default.default-Pc\AppData\Local\Microsoft\VisualStudio\SSDT\JobListeDAB\Joblistesystem.mdf");
+            //conn.Open();
             Table<User> users = db.GetTable<User>();
              /* Sammenligner email og password med databasen. Hvis der findes en bruger 
              * med matchende email og password returneres dennes bruger ID. Ellers returneres -1. */
---
+++ Projekt/JobListeDAB/DatabaseInterface/MaterialClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseInterface
+{
+    public class MaterialClass
+    {
+        public string MaterialType { get; set; }
+    }
+}
