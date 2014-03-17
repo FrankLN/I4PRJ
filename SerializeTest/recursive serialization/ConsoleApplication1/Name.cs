a0a8e3679c26330e9d5a1dc9f077539ffd69367c
Frank Laumann <frankder@hotmail.com>
2014-03-17 15:06:59 +0100

    Recursive serialization

---
+++ SerializeTest/recursive serialization/ConsoleApplication1/Name.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    public class Name : ISerializable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Name(string fName, string lName)
+        {
+            FirstName = fName;
+            LastName = lName;
+        }
+
+        public Name(SerializationInfo info, StreamingContext context)
+        {
+            FirstName = (string)info.GetValue("fName", typeof (string));
+            LastName = (string) info.GetValue("lName", typeof (string));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("fName", FirstName);
+            info.AddValue("lName", LastName);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
---
+++ SerializeTest/recursive serialization/ConsoleApplication1/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    public class User : ISerializable
+    {
+        private Name _name;
+
+        public User(Name name)
+        {
+            _name = name;
+        }
+
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            _name = (Name)info.GetValue("name", typeof(Name));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("name", _name);
+        }
+
+        public void printUser()
+        {
+            _name.PrintName();
+        }
+    }
+}
