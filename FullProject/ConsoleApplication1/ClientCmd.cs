2f4020b38ad7a5f5c17306bd583363e7bfb4f7c8
Lasse <lassebuesvendsen@gmail.com>
2014-03-21 19:58:33 +0100

    same

--- FullProject/ConsoleApplication1/ClientCmd.cs
+++ FullProject/ConsoleApplication1/ClientCmd.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using ClientApplication;
 using MessageTypes.ReplyMessages;
 
@@ -10,10 +11,18 @@ namespace ConsoleApplication1
 {
     public class ClientCmd : IClientCmd
     {
-        public delegate void mydelegate(string navn);
+        public delegate void mydelegate(IReplyMessage msg);
 
         public event mydelegate onReplyMsgReceived;
 
+        public void eventFire()
+        {
+            if (onReplyMsgReceived != null)
+            {
+                onReplyMsgReceived();
+                MessageBox.Show("Event fired");
+            }
+        }
 
 
         public void LoginVerification(ILoginReplyReplyMsg msg)
@@ -57,4 +66,5 @@ namespace ConsoleApplication1
 
         }
     }
+
 }
