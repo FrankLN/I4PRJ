2b0e360cd206f0262bb18af7e19996f208b132d2
lassebue <lassebuesvendsen@gmail.com>
2014-03-24 15:43:10 +0100

    more new stuff clientcmd

--- FullProject/MessageTypes/JobClass.cs
+++ FullProject/MessageTypes/JobClass.cs
@@ -15,34 +15,44 @@ namespace DatabaseInterface
         public string Deadline { get; set; }
         public UserClass Owner { get; set; }
         public string File { get; set; }
-        public string CreationTime { get; set; }
         public long FileSize { get; set; }
+        public string CreationTime { get; set; }
+        public int Hollow { get; set; }
+        public string Comment { get; set; }
 
         public JobClass()
         {
-            
+            OrderId = 0;
+            Material = new MaterialClass();
+            Deadline = "";
+            Owner = new UserClass();
+            File = "";
+            CreationTime = "";
+            Hollow = 0;             // Hollow is set to 0 as the 3DJob is full by default.
+            Comment = "";
         }
 
         public JobClass(SerializationInfo info, StreamingContext context)
         {
             Material = (MaterialClass)info.GetValue("Material", typeof(MaterialClass));
-            //Hollow = (bool)info.GetValue("Hollow", typeof(bool));
+            Hollow = (int)info.GetValue("Hollow", typeof(int));
+            FileSize = (long)info.GetValue("FileSize", typeof(long));
             Deadline = (string)info.GetValue("Deadline", typeof(string));
             File = (string)info.GetValue("File", typeof(string));
             Owner = (UserClass)info.GetValue("Owner", typeof(UserClass));
             CreationTime = (string)info.GetValue("CreationTime", typeof(string));
-            FileSize = (long)info.GetValue("FileSize", typeof(long));
+            Comment = (string)info.GetValue("Comment", typeof(string));
         }
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             info.AddValue("Material", Material);
-            //info.AddValue("Hollow", Hollow);
+            info.AddValue("Hollow", Hollow);
             info.AddValue("Deadline", Deadline);
             info.AddValue("File", File);
+            info.AddValue("FileSize", FileSize);
             info.AddValue("Owner", Owner);
             info.AddValue("CreationTime", CreationTime);
-            info.AddValue("FileSize", FileSize);
-
+            info.AddValue("Comment", Comment);
         }
     }
 }
