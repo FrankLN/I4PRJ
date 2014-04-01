ad9987dda2c8dfb36ce747ac74238f326448197d
jonasjuss <jonasjuss@gmail.com>
2014-04-01 11:53:53 +0200

    test

---
+++ FullProject/ClientApplication.Test/FakeClient.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessageTypes;
+using MessageTypes.Messages;
+using MessageTypes.ReplyMessages;
+using Server;
+
+namespace ClientApplication.Test
+{
+    public class FakeClient : IClient, IServerApp
+    {
+        public FakeClient(int port)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with port: " + port.ToString());
+        }
+        public void SendToServer(ISerializable message)
+        {
+            IMessage tmpMsg = (IMessage) message;
+            tmpMsg.Run(this);
+        }
+
+        public IReplyMessage ReceiveMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveFile(long fileSize, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendFile(long fileSize, string path)
+        {
+            MessageBox.Show("FakeClient: SendFile called");
+        }
+
+        public void VerifyLogin(ILoginMsg loginMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + loginMsg.Email + " " + loginMsg.Password);
+        }
+
+        public void CreateUser(ICreateUserMsg createUserMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + createUserMsg.User.FirstName + " " +createUserMsg.User.Email + " " + createUserMsg.User.AdminRights + " etc...");
+        }
+
+        public void CreateJob(ICreateJobMsg createJobMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + createJobMsg.Job.Deadline + " " +
+                            createJobMsg.Job.Material.MaterialType);
+        }
+
+        public void RequestJobs(IRequestJobsMsg requestJobsMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + requestJobsMsg.ToString());
+        }
+
+        public void DownloadJob(IDownloadJobMsg downloadJobMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + downloadJobMsg.FileName);
+        }
+
+        public void GetMaterials(IGetMaterialsMsg getMaterialsMsg)
+        {
+            MessageBox.Show("FakeClient: SendToServer called with parameters: " + getMaterialsMsg.ToString());
+        }
+
+        public void ActivationCodeRequest(IActivationCodeRequestMsg activationCodeRequestMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VerifyLogin(ILoginMsg loginMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateUser(ICreateUserMsg createUserMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateJob(ICreateJobMsg createJobMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RequestJobs(IRequestJobsMsg requestJobsMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadJob(IDownloadJobMsg downloadJobMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMaterials(IGetMaterialsMsg getMaterialsMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActivationCodeRequest(IActivationCodeRequestMsg activationCodeRequestMsg, IServer server)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FullProject/ConsoleApplication1/Client.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Resources;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using MessageTypes.ReplyMessages;
+
+namespace ClientApplication
+{
+    public interface IClient
+    {
+        void SendToServer(ISerializable message);
+        IReplyMessage ReceiveMessage();
+        void ReceiveFile(long fileSize,string fileName);
+        void SendFile(long fileSize, string path);
+
+    }
+    public class Client : IClient
+    {
+        //Memberdata...
+        private TcpClient clientSocket;
+        private NetworkStream outInStream;
+        private BinaryFormatter bFormatter;
+        private const int MaxPackageSize = 1000;
+        private int _port;
+
+        //Constructor which take ip and port belonging to the targeting server
+        public Client(int port)
+        {
+            Init(port);
+        }
+
+        //Init method setting the up the client 
+        private void Init(int port)
+        {
+            clientSocket = new TcpClient();
+            _port = port;
+            clientSocket.Connect("10.0.0.1", _port);
+            outInStream = clientSocket.GetStream();
+            bFormatter = new BinaryFormatter();
+        }
+
+        //Method for sending classobject
+        public void SendToServer(ISerializable objekt)
+        {
+
+            bFormatter.Serialize(outInStream, objekt);
+
+        }
+
+        public IReplyMessage ReceiveMessage()
+        {
+            IReplyMessage reply = (IReplyMessage)bFormatter.Deserialize(outInStream);
+            
+            return reply;
+        }
+
+        // Methode that receives files
+        public void ReceiveFile(long fileSize, string fileName)
+        {
+            int n;
+            var rest = fileSize;
+            var name = fileName.Substring(1+fileName.LastIndexOf(@"\"));
+            var pathAndName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+ "Downloads" + name;
+                
+            var downloadFile = new FileStream(pathAndName,FileMode.Create,FileAccess.Write);
+
+            var receiveBuffer = new byte[MaxPackageSize];
+            while (rest != 0)
+            {
+                n = outInStream.Read(receiveBuffer, 0, MaxPackageSize);
+                downloadFile.Write(receiveBuffer, 0, MaxPackageSize);
+
+                rest -= n;
+            }
+        }
+        // Methode that sends a file of a job
+        public void SendFile(long fileSize ,string path)
+        {
+            int n;
+            var rest = fileSize;
+            var sendBuffer = new byte[MaxPackageSize];
+            var uploadFile = new FileStream(path,FileMode.Open);
+            while (rest != 0)
+            {
+                n = uploadFile.Read(sendBuffer, 0, MaxPackageSize);
+                outInStream.Write(sendBuffer, 0, n);
+                rest -= n;
+            }
+        }
+    }
+}
