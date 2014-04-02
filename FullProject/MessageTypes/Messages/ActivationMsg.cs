67b801b0389f4cc41d375e3f3ab3e3da2c7d5f61
Bue <lassebuesvendsen@gmail.com>
2014-04-02 16:08:41 +0200

    mere Activation

---
+++ FullProject/MessageTypes/Messages/ActivationMsg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseInterface;
+using Server;
+
+namespace MessageTypes.Messages
+{
+    public interface IActivationMsg
+    {
+        UserClass User { get; }
+    }
+    class ActivationMsg : ISerializable, IMessage, IActivationMsg
+    {
+        public UserClass User { get; set; }
+
+        public ActivationMsg()
+        {
+            User = new UserClass();
+        }
+        public ActivationMsg(SerializationInfo info, StreamingContext context)
+        {
+            User = (UserClass)info.GetValue("User", typeof(UserClass));
+        }
+
+        public void Run(IServerApp serverApp)
+        {
+            serverApp.ActivateUser(this);
+        }
+
+        public void Run(IServerApp serverApp, IServer server)
+        {
+            serverApp.ActivateUser(this);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("User", User);
+        }
+    }
+}
---
+++ FullProject/MessageTypes/ReplyMessages/ActivationReplyMsg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+
+namespace MessageTypes.ReplyMessages
+{
+    public interface IActivationReplyMsg
+    {
+        bool UserActivated { get; }
+    }
+
+    class ActivationReplyMsg: ISerializable, IReplyMessage,IActivationReplyMsg
+    {
+        public bool UserAvctivated { set; get; }
+        public ActivationReplyMsg()
+        {
+            UserActivated = false;
+        }
+
+        public ActivationReplyMsg(SerializationInfo info, StreamingContext context)
+        {
+            UserActivated = (bool)info.GetValue("UserActivated", typeof(bool));
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("UserActivated", UserActivated);
+        }
+
+        public void Run(IClientCmd clientCmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserActivated { get; private set; }
+    }
+}
