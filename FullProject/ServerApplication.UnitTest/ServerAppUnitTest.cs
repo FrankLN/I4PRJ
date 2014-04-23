3749c1f4eedb9a5016819481dde031654fc79c33
Frank Laumann Nielsen <frankder@hotmail.com>
2014-04-23 13:27:38 +0200

    Server unit test

---
+++ FullProject/ServerApplication.UnitTest/ServerAppUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ServerApplication.UnitTest
+{
+    [TestFixture]
+    public class ServerAppUnitTest
+    {
+        [Test]
+        public void ServerApp_Constructor_expect()
+        {
+            
+        }
+    }
+}
