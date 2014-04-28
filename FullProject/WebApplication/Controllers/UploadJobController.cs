a1379366a8141ff9eacc34817df89731adcd080f
Soeren Emil Rasmussen <201270970@iha.dk>
2014-04-28 15:19:56 +0200

    UploadJob

---
+++ FullProject/WebApplication/Controllers/UploadJobController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class UploadJobController : Controller
+    {
+        //
+        // GET: /UploadJob/
+        public ActionResult UploadJob()
+        {
+            return View();
+        }
+	}
+}
