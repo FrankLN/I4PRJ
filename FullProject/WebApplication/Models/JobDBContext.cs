263a138ecff7ec43cee02b6b0375c7ccccd465b9
UlverHvidt <201270857@iha.dk>
2014-04-29 14:56:55 +0200

    DBContext klasser for materiale og job.

---
+++ FullProject/WebApplication/Models/JobDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DatabaseInterface;
+
+namespace WebApplication.Models
+{
+    public class JobDBContext : DbContext
+    {
+        DbSet<JobClass> Jobs { get; set; } 
+    }
+}
---
+++ FullProject/WebApplication/Models/MaterialDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DatabaseInterface;
+
+namespace WebApplication.Models
+{
+    public class MaterialDBContext : DbContext
+    {
+        DbSet<MaterialClass> Materials { get; set; } 
+    }
+}
