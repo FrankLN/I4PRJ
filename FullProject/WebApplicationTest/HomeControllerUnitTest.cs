df2d3c3b70d05558369f47f8ec7ab1e3435a731c
Frank Laumann Nielsen <frankder@hotmail.com>
2014-05-20 17:31:57 +0200

    Web Unit test

---
+++ FullProject/WebApplicationTest/HomeControllerUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using NUnit.Framework;
+using WebApplication.Controllers;
+
+namespace WebApplicationTest
+{
+    [TestFixture]
+    class HomeControllerUnitTest
+    {
+        [Test]
+        public void HomeController_Index_ReturnIndexView()
+        {
+            string expected = "Index";
+
+            HomeController t = new HomeController();
+
+            ActionResult a = t.Index();
+
+            string actual = ((ViewResult) a).ViewName;
+
+
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
